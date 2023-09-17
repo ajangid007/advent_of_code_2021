2bc772a6cac5ecb340a7c152dea0a30147a13833
ajangid007 <anuradha.jangid@gmail.com>
2023-09-17 15:35:29 -0700

    day8

---
+++ day8/part1.py
@@ -0,0 +1,39 @@
+import os
+import sys
+
+
+def readInput(inputfile):
+    lines =  open (os.path.join(sys.path[0],inputfile), "r")  
+    return [line.strip() for line in lines.readlines()]
+
+class Digits:
+    def __init__(self,data):
+        self.signals = data
+        self.unique_digits = {
+            1: 2,
+            4: 4,
+            7: 3,
+            8: 7
+        }
+   
+    def count(self,digit):
+        count = 0
+        for row in self.signals:
+            input, output  = row.split("|")
+            inputs = input.strip().split(" ")
+            outputs = output.strip().split(" ")
+            for item in outputs:
+                if len(item) == self.unique_digits[digit]:
+                    count += 1
+        return count
+
+
+
+if __name__ == '__main__':
+    data = readInput('input.txt')
+    digits = Digits(data)
+    print("1: "+ str(digits.count(1)) + ", 4: " + str(digits.count(4)) + ", 7: " + str(digits.count(7)) + ", 8: " + str(digits.count(8)))
+    print(digits.count(1)+digits.count(4)+digits.count(7)+digits.count(8))
+
+
+
---
+++ day8/part2.py
@@ -0,0 +1,73 @@
+import os
+import sys
+import itertools
+
+
+def readInput(inputfile):
+    lines = open (os.path.join(sys.path[0],inputfile), "r")
+    return [line.strip() for line in lines.readlines()]
+
+class Digits:
+    def __init__(self,data):
+        self.signals = data
+        self.digitkeys = [ 
+            "abcefg",
+            "cf",
+            "acdeg",
+            "acdfg",
+            "bcdf",
+            "abdfg",
+            "abdefg",
+            "acf",
+            "abcdefg",
+            "abcdfg"
+        ]
+        self.MappedDigits = {
+            "abcefg": 0,
+            "cf":1,
+            "acdeg":2,
+            "acdfg":3,
+            "bcdf":4,
+            "abdfg":5,
+            "abdefg":6,
+            "acf":7,
+            "abcdefg":8,
+            "abcdfg":9
+        }
+
+    def countAll(self):
+        sum = 0 
+        for line in self.signals:
+            inputs, outputs = line.strip().split("|")
+            for sigma in list(itertools.permutations('abcdefg')):
+                key = {}
+                for c in "abcdefg":
+                    key[c] = sigma["abcdefg".index(c)]
+
+                mappedinputs = []
+
+                for input in inputs.strip().split(" "):
+                    digit = ""
+                    for x in input:
+                        digit += key[x] 
+                    mappedinputs.append("".join(sorted(digit)))
+
+                if all(elem in self.digitkeys for elem in mappedinputs):
+                    outputNumber = 0
+                    for output in outputs.strip().split(" "):
+                        number = ""
+                        for c in output:
+                            number += key[c]
+                        outputNumber = outputNumber*10 + self.MappedDigits["".join(sorted(number))]
+                    sum += outputNumber
+                    break
+                else:
+                    continue 
+        print(sum)
+
+
+
+if __name__ == '__main__':
+    data = readInput('input.txt')
+    digits = Digits(data)
+    digits.countAll()
