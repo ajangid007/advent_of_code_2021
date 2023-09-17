5d86f58417a62155198087f13c41cbcf57e78edf
ajangid007 <anuradha.jangid@gmail.com>
2023-09-17 14:54:55 -0700

    fixes for puzzle 1 part 2

--- puzzle1/measurement_sliding_window.py
+++ puzzle1/measurement_sliding_window.py
@@ -7,30 +7,8 @@ def readInput(inputfile):
         return [int(x) for x in myfile.read().split()]
     
 def measureSlidingWindow(input):
-    max = 0
-    measurement = 0
-    windows = queue.Queue(maxsize=3)
-    for i in range(4):
-        windows.put(queue.Queue(maxsize=3))
-    for value in input:
-        count = 3
-
-        while not windows.empty():
-            window = windows.get()
-            if window.full():
-                compare  = sum(window)
-                if compare > max and max != 0:
-                    measurement += 1
-                    max = compare
-                window.clear()
-                windows.put(window)
-            if count > 0:
-                window.put(value)
-                count -= 1
-            else:
-                break
-    return measurement
-
+    sums = sum(x < y for x, y in zip(input, input[3:]))
+    print(sums)
 
 if __name__ == '__main__':
     input = readInput("input.txt")
